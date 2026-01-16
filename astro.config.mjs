f4374775ca6d0c849be695c50742d72fef582e11
David Exiga <edavid9@gatech.edu>
2026-01-16 00:57:32 -0600

    initial site

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+export default {
+  site: 'https://moisesautobodyandpaint.com',
+  base: '/',
+  output: 'static',
+  integrations: [],
+  markdown: {
+    shikiConfig: {
+      theme: 'nord',
+    },
+  },
+};
